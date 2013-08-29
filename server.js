9f207397d9dfceaf01d1a52407a9e82024eaa194
Art <github@darkruby.co.uk>
2013-08-29 23:14:36 +0100

    re-added animate, changed opensource text, changed SO link and icon, updated server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@
   require('http').createServer(function (request, response) {
       request.addListener('end', function () {
           file.serve(request, response);
-      });
+      }).resume();
   }).listen(port);
 
   console.log('listening on port ', port);
